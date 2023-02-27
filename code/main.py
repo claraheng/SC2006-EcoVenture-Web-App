b467229b7611ef964a9fecbfaa8d8b931f1b527f
LinLaoBei <lzyleezhiyong@gmail.com>
2023-02-28 00:49:20 +0800

    change index to display my location and add a template home page

--- code/main.py
+++ code/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route('/location')
+def get_location():
+    if request.headers.get('X-Forwarded-For'):
+        ip = request.headers.get('X-Forwarded-For').split(',')[0]
+    else:
+        ip = request.remote_addr
+
+    return render_template('location.html', ip=ip)
 
 if __name__ == "__main__":
    port = int(os.environ.get("PORT", 5000))

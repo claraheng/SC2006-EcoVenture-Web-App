e5ca991b4e17e322b3e9aa6de4e5c063a21fd742
LinLaoBei <lzyleezhiyong@gmail.com>
2023-03-01 23:36:19 +0800

    added more stuff

--- code/main.py
+++ code/main.py
@@ -1,24 +1,54 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, redirect, session, url_for 
 import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-@app.route("/")
+#static definition of users for testing purposes 
+users = {
+    'user1': 'password1',
+    'user2': 'password2'
+}
+
+
+@app.route("/") #default page
+def login():
+    return render_template("login.html") 
+
+
+@app.route("/checkin") #get current location and display on map 
 def index():
-    return render_template("index.html")
+    return render_template("checkin.html")
 
-@app.route("/home")
+@app.route("/home") #home page before login 
 def home():
     return render_template("home.html")
 
-@app.route('/location')
-def get_location():
-    if request.headers.get('X-Forwarded-For'):
-        ip = request.headers.get('X-Forwarded-For').split(',')[0]
-    else:
-        ip = request.remote_addr
+@app.route("/user") #userpage
+def user():
+    # get the username from the session
+    username = session.get('username')
+    # if the user is not logged in, redirect to the login page
+    if not username:
+        return redirect(url_for('login'))
+    return render_template('user.html', username=username)
 
-    return render_template('location.html', ip=ip)
+
+@app.route('/login', methods=['POST']) #can only access this route via POST request 
+def authenticate():
+    # check if the username and password are correct
+    if request.form['username'] in users and request.form['password'] == users[request.form['username']]:
+        # store the username in the session
+        session['username'] = request.form['username']
+        return redirect(url_for('user'))
+    else:
+        # if the username or password is incorrect, show an error message
+        return render_template('login.html', error='Invalid username or password')
+    
+# General error handler for all status codes
+@app.errorhandler(Exception)
+def handle_error(e):
+    return render_template('error.html', error=str(e)), getattr(e, 'code', 500)
 
 if __name__ == "__main__":
    port = int(os.environ.get("PORT", 5000))

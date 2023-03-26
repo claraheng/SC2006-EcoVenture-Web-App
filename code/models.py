927edc3a8190f2c0718bf1b8cc3c3688084413ca
Clara Heng <c@claraheng.com>
2023-03-26 13:27:31 +0800

    fixed flask login + added react login link

--- code/models.py
+++ code/models.py
@@ -7,6 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/zhiyonglee/Documents/
 app.config['SQLALCHEMY_BINDS'] = {
     'users': 'sqlite://///Users/zhiyonglee/Documents/GitHub/sc2006lab/code/DB/users.db',
     'fitnessareas': 'sqlite://///Users/zhiyonglee/Documents/GitHub/sc2006lab/code/DB/areas.db'
+    # 'users': 'sqlite://///Users/claraheng/Library/CloudStorage/OneDrive-NanyangTechnologicalUniversity/School/_S2_AY_22_23/SC2006 SOFTWARE ENGINEERING/Group 5 Maggie/SC2006repo/sc2006lab/code/DB/users.db',
+    # 'fitnessareas': 'sqlite://///Users/claraheng/Library/CloudStorage/OneDrive-NanyangTechnologicalUniversity/School/_S2_AY_22_23/SC2006 SOFTWARE ENGINEERING/Group 5 Maggie/SC2006repo/sc2006lab/code/DB/areas.db'
 }
 db = SQLAlchemy(app)
 

1abba9e80d0bbe04c2014f948b4f295c17f94382
LinLaoBei <lzyleezhiyong@gmail.com>
2023-03-28 00:41:37 +0800

    added display of all fitness area on admin map

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, session, url_for,flash, Fl
 from flask_login import login_required
 from user import createAccount
 from auth import auth_bp, login_manager
+from fitnessareas import areas_bp
 from whereshouldigo import WhereShouldIGo_bp
 from checkin import checkin_bp
 import sqlite3
@@ -18,6 +19,7 @@ app.secret_key = 'your_secret_key'
 app.register_blueprint(auth_bp)
 app.register_blueprint(checkin_bp)
 app.register_blueprint(WhereShouldIGo_bp)
+app.register_blueprint(areas_bp)
 login_manager.init_app(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
